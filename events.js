90936d1e7890e4f1f4b7138b5826f142063d98eb
Li Weinan <liweinan0423@icloud.com>
2015-02-25 19:20:03 +0800

    Update events.js

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
 
 	var el = document.createElement('script');
-	el.src='https://raw.github.com/liweinan0423/Objectiva_Time_Calculate/master/time_calculate.js';
+	el.src='https://raw.github.com/liweinan0423/weekly-hours/master/time_calculate.js';
 	el.type="text/javascript";
 	document.body.appendChild(el);
